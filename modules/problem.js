395639f964a6f6dff7493ca8ebc36f3309bcc156
小灰 <xiaohui@evecat.dev>
2026-02-07 23:45:54 +0800

    🐾 restrict submission visibility and testdata downloads

--- modules/problem.js
+++ modules/problem.js
@@ -938,8 +938,9 @@ app.get('/problem/:id/testdata', async (req, res) => {
 
     if (!problem) throw new ErrorMessage('无此题目。');
     if (!await problem.isAllowedUseBy(res.locals.user)) throw new ErrorMessage('您没有权限进行此操作。');
-    if (await res.locals.user.isStudent()) throw new ErrorMessage('您没有权限进行此操作。');
-    if (await res.locals.user.isTeacher()) throw new ErrorMessage('您没有权限进行此操作。'); 
+    if (!res.locals.user || (res.locals.user.user_type !== 'admin' && res.locals.user.user_type !== 'lecturer')) {
+      throw new ErrorMessage('您没有权限进行此操作。');
+    }
 
     let testdata = await problem.listTestdata();
     let testcases = await syzoj.utils.parseTestdata(problem.getTestdataPath(), problem.type === 'submit-answer');
@@ -1024,7 +1025,9 @@ app.get('/problem/:id/testdata/download/:filename?', async (req, res) => {
 
     if (!problem) throw new ErrorMessage('无此题目。');
     if (!await problem.isAllowedUseBy(res.locals.user)) throw new ErrorMessage('您没有权限进行此操作。');
-    if (await res.locals.user.isStudent()) throw new ErrorMessage('您没有权限进行此操作。'); 
+    if (!res.locals.user || (res.locals.user.user_type !== 'admin' && res.locals.user.user_type !== 'lecturer')) {
+      throw new ErrorMessage('您没有权限进行此操作。');
+    }
     if (typeof req.params.filename === 'string' && (req.params.filename.includes('../'))) throw new ErrorMessage('您没有权限进行此操作。)');
 
     if (!req.params.filename) {
